d4bb3b7b416cd600efedc755a43e9515660ec08d
MavenHive <subscriptions@mavenhive.in>
2018-09-09 16:53:02 +0700

    Single entity

---
+++ src/main/java/com/example/college/Student.java
@@ -0,0 +1,42 @@
+package com.example.college;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String address;
+
+    public Student(){};
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
